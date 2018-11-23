50e802ac870cc3629b2914f78881dbef94b49d6c
Ivan*2Ko <ivans.kope@gmail.com>
2018-11-23 17:58:54 +0300

    Google OAuth20 flow implemented

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const express = require('express');
 const router = require('./router');
 const morgan = require('morgan');
@@ -11,12 +12,21 @@ const path = require('path');
 const app = express();
 const DB = 'aaa';
 
+const secrets = require('./secrets');
+
 app.use(compression());
 app.use(cookieParser());
 app.use(morgan('combined'));
 app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 app.use(express.static(path.join(__dirname, '../static/build')));
+app.use(require('express-session')({
+  resave: true,
+  saveUninitialized: true,
+  secret: secrets.AAA_SESSION_SECRET,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(router);
 
